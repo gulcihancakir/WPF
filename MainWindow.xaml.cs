8b2e1c077b06cfdda95f2187c15efabc0f6f633c
gulcihancakir <gulcihancakir8@gmail.com>
2020-04-04 01:43:31 +0300

    duzenleme

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,8 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int i;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,5 +32,45 @@ namespace WPF
         {
 
         }
+
+        //private void yeni_cikan_Click(object sender, RoutedEventArgs e)
+        //{
+
+           
+
+        //}
+
+        private void goBack(object sender, RoutedEventArgs e)
+        {
+            i--; // this will decrease 1 from i
+
+
+            // if the value of i is less than 1
+            // then give i the value of 6
+            if (i < 1)
+            {
+                i = 3;
+            }
+            picHolder.Source = new BitmapImage(new Uri(@"pics/" + i + ".jpg", UriKind.Relative));
+        }
+
+        private void goNext(object sender, RoutedEventArgs e)
+        {
+
+            i++; // increase i by 1
+
+            // if i's value gets larger than 6 then reset i back to 1
+
+            if (i > 3)
+            {
+                i = 1;
+            }
+
+            // change the picture according to the i's value
+            picHolder.Source = new BitmapImage(new Uri(@"pics/" + i + ".jpg", UriKind.Relative));
+            Console.WriteLine("hkweşlsözxöcvmbşk");
+
+        }
     }
-}
+    }
+
